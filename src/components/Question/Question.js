1b629c6f00b053ecc3fc9e6b64835cdabab32fd5
Conor <67439130+conorkenahan@users.noreply.github.com>
2021-04-16 17:56:31 -0400

    decodeURI showing undefined for a split second on question screen.

--- src/components/Question/Question.js
+++ src/components/Question/Question.js
@@ -5,14 +5,22 @@ const Question = ({ question }) => {
     let answers = question.incorrect_answers;
     answers && answers.push(question.correct_answer);
     let randomizedAnswers = answers && answers[Math.floor(Math.random()*answers.length)];
-    console.log(answers);
+    let newAnswers = [...new Set(answers)]
+    let questionTitle = decodeURIComponent(question.question);
+
     
     return (
         <div>
-            <h3>{question && decodeURIComponent(question.question)}</h3>
-            {answers && answers.map(answer => {
-                return <p>{decodeURIComponent(answer)}</p>
-            })}
+            {!question ? <h3>Loading...</h3> : 
+            <div>
+                <h3>
+                    {questionTitle}
+                </h3>
+                    {newAnswers.map(answer => {
+                        return <p>{decodeURIComponent(answer)}</p>
+                })}
+            </div>
+            }
         </div>
     )
 }
